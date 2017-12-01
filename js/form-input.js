911703237462bcf6293d524b648c0da3a8cdce7a
chris turina <chris.turina@gmail.com>
2017-12-01 14:49:12 -0800

    serches names

--- js/form-input.js
+++ js/form-input.js
@@ -3,20 +3,20 @@ var apiKey = require('./../.env').apiKey;
 
 export class Doctor {
   constructor() {
-
+    this.doc;
   }
 
   getName(name) {
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?limit=10&user_key=92eabac06f3099882483789a6b1b7ad9&name&query=&location=45.5231, -122.6765, 100`).then(function(responce) {
-      for (let i = 0; i < 10; i++){
-        $('.doctor-output').append(`<img src="${responce.data[i].profile.image_url}" alt="image of ${responce.data[i].profile.first_name} ${responce.data[i].profile.last_name}"> <div class='new-doc-info'><p class='doc-name'>${responce.data[i].profile.first_name} ${responce.data[i].profile.last_name}</p> <p class='doc-adress'>${responce.data[i].practices[i].visit_address.city}, ${responce.data[i].practices[i].visit_address.state} ${responce.data[i].practices[i].visit_address.street} ${responce.data[i].practices[i].visit_address.zip}</p> <p class='doc-phone'>${responce.data[i].practices[i].phones[i].number}</p> <p class='doc-website'>${responce.data[i].practices[i].website}</p></div></div>`);
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?limit=20&user_key=92eabac06f3099882483789a6b1b7ad9&name=${name}&query=&location=45.5231, -122.6765, 200`).then(function(responce) {
+      console.log(`${name}`);
+      for (let i = 0; i < 20; i++){
+        $('.doctor-output').append(`<div class='new-doc-info'><p class='doc-name'>${responce.data[i].profile.first_name} ${responce.data[i].profile.last_name}</p></div>`);
 
 
       };
     }).fail(function(error) {
       alert('try again');
     });
-
   }
 
   // getName(name) {
--- js/js-interface.js
+++ js/js-interface.js
@@ -1,10 +1,16 @@
 import { Doctor } from "./../js/form-input.js";
 
 $(document).ready(function() {
+  let doctorName = new Doctor();
   $("#button").click(function(event) {
     event.preventDefault();
-    let doctorName = new Doctor();
     let nameInput = $('#input').val();
     doctorName.getName(nameInput);
   });
+
+  $('.new-doc-info').click(function(event) {
+    event.preventDefault();
+    let docName = $(event.target).text();
+    console.log("docName");
+  })
 });
