7b8c896b955c9eeba1d9b45a61c4a82174d5bd9a
chris turina <chris.turina@gmail.com>
2017-12-01 16:28:55 -0800

    can if a valid name was entered

--- js/form-input.js
+++ js/form-input.js
@@ -8,6 +8,7 @@ export class Doctor {
 
   getName(name) {
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?limit=10&user_key=92eabac06f3099882483789a6b1b7ad9&name=${name}&query=&location=45.5231, -122.6765, 200`).then(function(responce) {
+      if (responce.data.length > 0 ){
       for (let i = 0; i < responce.data.length; i++){
         $('.doctor-output').append(`<div class='new-doc-info'> <img src="${responce.data[i].profile.image_url}" alt="image of ${responce.data[i].profile.first_name} ${responce.data[i].profile.last_name}"> <p class='doc-name'>${responce.data[i].profile.first_name} ${responce.data[i].profile.last_name}</p></div>`);
         for (let j =0; j < responce.data[i].practices.length; j++) {
@@ -25,44 +26,17 @@ export class Doctor {
           }
         }
 
+      }
+
+    } else {
+      console.log('make it');
+      $('.doctor-output').append(`<p>No Doctor Exists with that Name</p>`)
 
       };
     }).fail(function(error) {
-      alert('try again');
+      alert('There was an error on our part try again');
     });
   }
 
-  // getName(name) {
-  //   $.get(`https://api.betterdoctor.com/2016-03-01/doctors?limit=10&user_key=92eabac06f3099882483789a6b1b7ad9&name&query=&location=45.5231, -122.6765, 100`).then(function(responce) {
-  //     for (let i = 0; i < 10; i++){
-  //       $('.doctor-output').append(`<img src="${responce.data[i].profile.image_url}" alt="image of ${responce.data[i].profile.first_name} ${responce.data[i].profile.last_name}"> <div class='new-doc-info'><p class='doc-name'>${responce.data[i].profile.first_name} ${responce.data[i].profile.last_name}</p> </div></div>`);
-  //
-  //
-  //     };
-  //   }).fail(function(error) {
-  //     alert('try again');
-  //   });
-  //
-  // }
-
-//   getAdress(name) {
-//     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?limit=10&user_key=92eabac06f3099882483789a6b1b7ad9&name&query=&location=45.5231, -122.6765, 100`).then(function(responce) {
-//       for (let i = 0; i < 10; i++){
-//         $('.doctor-output').append(`<p class='doc-adress'>${responce.data[i].practices[i].visit_address.city}, ${responce.data[i].practices[i].visit_address.state} ${responce.data[i].practices[i].visit_address.street} ${responce.data[i].practices[i].visit_address.zip}</p>`);
-//       };
-//     }).fail(function(error) {
-//       alert('no address');
-//     });
-//   }
-// }
-
-// getPhone(name) {
-//   $.get(`https://api.betterdoctor.com/2016-03-01/doctors?limit=10&user_key=92eabac06f3099882483789a6b1b7ad9&name&query=&location=45.5231, -122.6765, 100`).then(function(responce) {
-//     for (let i = 0; i < 10; i++){
-//       $('.doctor-output').append(`<p class='doc-phone'>${responce.data[i].practices[i].phones[i].number}</p> <p class='doc-website'>${responce.data[i].practices[i].website}</p></div></div>`);
-//     };
-//   }).fail(function(error) {
-//     alert('try again');
-//   });
-// }
+  
 }
