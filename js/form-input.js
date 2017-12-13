41e74deea2132d1bf0f306ae355a3c011b6d451d
chris turina <chris.turina@gmail.com>
2017-12-13 06:25:42 -0800

    seperated business logic and user interface

--- js/form-input.js
+++ js/form-input.js
@@ -5,65 +5,17 @@ export class Doctor {
   constructor() {
     this.doc;
   }
-
-  getName(name) {
+  getName(name, showData, error) {
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?limit=10&user_key=92eabac06f3099882483789a6b1b7ad9&name=${name}&query=&location=45.5231, -122.6765, 200`).then(function(response) {
-      if (response.data.length > 0 ){
-        for (let i = 0; i < response.data.length; i++){
-          $('.doctor-output').append(`<div class='new-doc-info'> <img src="${response.data[i].profile.image_url}" alt="image of ${response.data[i].profile.first_name} ${response.data[i].profile.last_name}"> <p class='doc-name'>${response.data[i].profile.first_name} ${response.data[i].profile.last_name}</p></div>`);
-          for (let j =0; j < response.data[i].practices.length; j++) {
-            $('.doctor-output').append(`${response.data[i].practices[j].visit_address.city}, ${response.data[i].practices[j].visit_address.state} ${response.data[i].practices[j].visit_address.street} ${response.data[i].practices[j].visit_address.zip}`);
-            $('.doctor-output').append(`<p class='doc-phone'>${response.data[i].practices[j].phones[0].number}</p>`);
-            if(response.data[i].practices[j].website != undefined) {
-              $('.doctor-output').append(`<p>${response.data[i].practices[j].website}</p>`);
-            } else {
-              $('.doctor-output').append(`<p>No website Availible</p>`)
-            };
-            if(response.data[i].practices[j].accepts_new_patients === true) {
-              $('.doctor-output').append(`<p>Currently Accepting New Patients </p>`);
-            } else {
-              $('.doctor-output').append(`<p>Not Currently Accepting New Patients</p>`)
-            }
-          }
-
-        }
-
-      } else {
-        console.log('make it');
-        $('.doctor-output').append(`<p>No Doctor Exists with that Name</p>`)
-
-      };
+      showData(response)
     }).fail(function(error) {
-      alert('There was an error on our part try again');
+      error(error)
     });
   }
 
-  getSymptom(symptom) {
+  getSymptom(symptom, showDataSymptom, error) {
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?limit=20&user_key=92eabac06f3099882483789a6b1b7ad9&query=${symptom}&location=45.5231, -122.6765, 200`).then(function(response) {
-      if (response.data.length > 0 ){
-        for (let i = 0; i < response.data.length; i++){
-          $('.doctor-output').append(`<div class='new-doc-info'> <img src="${response.data[i].profile.image_url}" alt="image of ${response.data[i].profile.first_name} ${response.data[i].profile.last_name}"> <p class='doc-name'>${response.data[i].profile.first_name} ${response.data[i].profile.last_name}</p></div>`);
-          for (let j =0; j < response.data[i].practices.length; j++) {
-            $('.doctor-output').append(`${response.data[i].practices[j].visit_address.city}, ${response.data[i].practices[j].visit_address.state} ${response.data[i].practices[j].visit_address.street} ${response.data[i].practices[j].visit_address.zip}`);
-            $('.doctor-output').append(`<p class='doc-phone'>${response.data[i].practices[j].phones[0].number}</p>`);
-            if(response.data[i].practices[j].website != undefined) {
-              $('.doctor-output').append(`<p>${response.data[i].practices[j].website}</p>`);
-            } else {
-              $('.doctor-output').append(`<p>No website Availible</p>`)
-            };
-            if(response.data[i].practices[j].accepts_new_patients === true) {
-              $('.doctor-output').append(`<p>Currently Accepting New Patients </p>`);
-            } else {
-              $('.doctor-output').append(`<p>Not Currently Accepting New Patients</p>`)
-            }
-          }
-
-        }
-
-      } else {
-        $('.doctor-output').append(`<p>No Doctor Exists with that Name</p>`)
-
-      };
+      showDataSymptom(response)
     }).fail(function(error) {
       alert('There was an error on our part try again');
     });
--- js/js-interface.js
+++ js/js-interface.js
@@ -1,16 +1,78 @@
 import { Doctor } from "./../js/form-input.js";
 
+let showData = function(response) {
+  if (response.data.length > 0 ){
+    for (let i = 0; i < response.data.length; i++){
+      $('.doctor-output').append(`<div class='new-doc-info'> <img src="${response.data[i].profile.image_url}" alt="image of ${response.data[i].profile.first_name} ${response.data[i].profile.last_name}"> <p class='doc-name'>${response.data[i].profile.first_name} ${response.data[i].profile.last_name}</p></div>`);
+      for (let j =0; j < response.data[i].practices.length; j++) {
+        $('.doctor-output').append(`${response.data[i].practices[j].visit_address.city}, ${response.data[i].practices[j].visit_address.state} ${response.data[i].practices[j].visit_address.street} ${response.data[i].practices[j].visit_address.zip}`);
+        $('.doctor-output').append(`<p class='doc-phone'>${response.data[i].practices[j].phones[0].number}</p>`);
+        if(response.data[i].practices[j].website != undefined) {
+          $('.doctor-output').append(`<p>${response.data[i].practices[j].website}</p>`);
+        } else {
+          $('.doctor-output').append(`<p>No website Availible</p>`)
+        };
+        if(response.data[i].practices[j].accepts_new_patients === true) {
+          $('.doctor-output').append(`<p>Currently Accepting New Patients </p>`);
+        } else {
+          $('.doctor-output').append(`<p>Not Currently Accepting New Patients</p>`)
+        }
+      }
+    }
+  } else {
+    $('.doctor-output').append(`<p>No Doctor Exists with that Name</p>`)
+  };
+}
+
+let showDataSymptom = function(response) {
+  if (response.data.length > 0 ){
+    for (let i = 0; i < response.data.length; i++){
+      $('.doctor-output').append(`<div class='new-doc-info'> <img src="${response.data[i].profile.image_url}" alt="image of ${response.data[i].profile.first_name} ${response.data[i].profile.last_name}"> <p class='doc-name'>${response.data[i].profile.first_name} ${response.data[i].profile.last_name}</p></div>`);
+      for (let j =0; j < response.data[i].practices.length; j++) {
+        $('.doctor-output').append(`${response.data[i].practices[j].visit_address.city}, ${response.data[i].practices[j].visit_address.state} ${response.data[i].practices[j].visit_address.street} ${response.data[i].practices[j].visit_address.zip}`);
+        $('.doctor-output').append(`<p class='doc-phone'>${response.data[i].practices[j].phones[0].number}</p>`);
+        if(response.data[i].practices[j].website != undefined) {
+          $('.doctor-output').append(`<p>${response.data[i].practices[j].website}</p>`);
+        } else {
+          $('.doctor-output').append(`<p>No website Availible</p>`)
+        };
+        if(response.data[i].practices[j].accepts_new_patients === true) {
+          $('.doctor-output').append(`<p>Currently Accepting New Patients </p>`);
+        } else {
+          $('.doctor-output').append(`<p>Not Currently Accepting New Patients</p>`)
+        }
+      }
+
+    }
+
+  } else {
+    $('.doctor-output').append(`<p>No Doctor Exists with that Name</p>`)
+
+  };
+}
+
+  let error = function(error) {
+    alert('There was an error on our part try again');
+  }
+
+
 $(document).ready(function() {
   let doctorName = new Doctor();
   $("#button").click(function(event) {
     event.preventDefault();
     let nameInput = $('#input').val();
-    doctorName.getName(nameInput);
-  });
+    doctorName.getName(nameInput, showData, error);
+
+
+
+});
+
+
+
 
   $("#symptom-button").click(function(event) {
     event.preventDefault();
     let symptomInput = $('#input-symptom').val();
-    doctorName.getSymptom(symptomInput);
+    doctorName.getSymptom(symptomInput, showDataSymptom, error);
   });
 });
